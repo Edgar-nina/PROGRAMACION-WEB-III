ae54ecac974fc8e7061871cf5bca67d73fba7ebe
Edgar-nina <edgarnina154@gmail.com>
2025-09-24 05:30:18 -0400

    Update e12.js

--- Practica #1/e12.js
+++ Practica #1/e12.js
@@ -1,25 +1,40 @@
-function paso1(callback) {
-    setTimeout(() => callback("Paso 1 completado"), 1000);
+function p1(callback) {
+    setTimeout(() => {
+        console.log("p1 c");
+        callback(5);
+    }, 1000);
 }
-
-function paso2(callback) {
-    setTimeout(() => callback("Paso 2 completado"), 1000);
+function p2(numero, callback) {
+    setTimeout(() => {
+        console.log("P2 c");
+        callback(numero * 2);
+    }, 1000);
 }
-paso1(r1 => {
-    console.log(r1);
-    paso2(r2 => {
-        console.log(r2);
-        console.log("Todos los pasos completados");
+p1(function(result1) {
+    p2(result1, function(result2) {
+        console.log("res", result2);
     });
 });
-
-
-//usando async y await
-async function ejecutar() {
-    console.log(await paso1());
-    console.log(await paso2());
-    console.log("Todos los pasos completados");
+/////////////
+function p1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("p1 c");
+            resolve(5);
+        }, 1000);
+    });
 }
-
+function p2(numero) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("p2 c");
+            resolve(numero * 2);
+        }, 1000);
+    });
+}
+async function ejecutar() {
+    const result1 = await p1();
+    const result2 = await p2(result1);
+    console.log("res final:", result2);
+}
 ejecutar();
-
